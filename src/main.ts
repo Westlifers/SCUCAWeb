c9a8d1e21ad363aba4c93d37c5c670d2cb9450a7
Westlifers <2585687459@qq.com>
2023-03-17 22:06:49 +0800

    公告支持katex

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
+import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
 
 // highlight.js
 import hljs from 'highlight.js';
@@ -19,9 +20,11 @@ import hljs from 'highlight.js';
 VMdEditor.use(githubTheme, {
     Hljs: hljs,
 });
+VMdEditor.use(createKatexPlugin());
 VMdPreview.use(githubTheme, {
     Hljs: hljs,
 });
+VMdPreview.use(createKatexPlugin());
 
 const app = createApp(App)
 app.use(ElementPlus)
