b3895db9dc2b3d15670df1b599c44e2f1e00c436
Westlifers <2585687459@qq.com>
2023-04-22 17:10:41 +0800

    修改布局，适应远程wss连接

--- src/views/room/roomSuspense.vue
+++ src/views/room/roomSuspense.vue
@@ -6,12 +6,12 @@
 
       <div class="room_left_bar__player_list">
         <div class="room_left_bar__player_list__title">
-          <p>Players</p>
+          <p>玩家列表</p>
         </div>
         <div class="room_left_bar__player_list__list">
           <el-scrollbar>
-            <div v-for="player in playerList" :key="player" class="room_left_bar__player_list__list__player">
-              <p>{{player}}</p>
+            <div v-for="player in playerList.slice(0, playerList.length-1)" :key="player" class="room_left_bar__player_list__list__player">
+              <p>{{player}}: {{playerResults[player]}}</p>
             </div>
           </el-scrollbar>
         </div>
@@ -31,24 +31,58 @@
           </el-scrollbar>
         </div>
         <div class="room_left_bar__chat__input">
-          <el-input v-model="message" placeholder="输入消息" @keydown.enter="send_chat_message" />
+          <el-input v-model="message" placeholder="输入消息" @keydown.enter="send_chat_message" @focus="is_typing=true" @blur="is_typing=false" />
         </div>
       </div>
 
     </div>
 
+    <el-divider direction="vertical" style="height: 100%" />
 
-    <div class="room_header">
-      <div class="room_header__left">
-        <p>{{roomId}}</p>
-        <p>{{event}}</p>
+    <div class="room_right_container">
+      <div class="room_header">
+        <div class="room_header__right">
+          <p>{{scramble}}</p>
+        </div>
+        <el-divider direction="vertical"/>
+        <div class="room_header__left">
+          <p>{{roomId}}</p>
+          <p>{{event}}</p>
+        </div>
       </div>
-      <div class="room_header__right">
-        {{scramble}}
+
+      <div class="room_main">
+        <div class="room_main__time">
+          <p>{{time===0?'DNF':time_convert(time)}}</p>
+        </div>
+        <div class="room_main__scramble_img">
+          <twisty-player
+              v-if="imgVisible"
+              :puzzle="translateEvent(event)"
+              :alg="scramble"
+              :visualization="is3d? '3D' : '2D'"
+          />
+          <div class="scramble-img-select">
+            <el-button
+                type="primary"
+                @click="imgVisible = !imgVisible"
+            >
+              {{imgVisible ? 'Hide' : 'Show'}} Scramble
+            </el-button>
+            <el-button
+                type="primary"
+                @click="is3d = !is3d"
+            >
+              {{is3d ? '2D' : '3D'}} Visualization
+            </el-button>
+          </div>
+        </div>
       </div>
     </div>
 
   </div>
+
+  <timing-curtain :state="timingState" @timing-over="set_time" v-if="!finished && !is_typing" />
 </template>
 
 <script lang="ts" setup>
@@ -105,18 +139,35 @@
 //       'players': ['Alice', 'Bob', 'Carol'],
 //   }
 import router from "@/router";
-import {Ref, ref} from "vue";
+import {Ref, ref, watch} from "vue";
 import {ElMessage} from "element-plus";
+import TimingCurtain from "@/components/timingCurtain/timingCurtain.vue";
+import {time_convert, translateEvent} from "@/utils";
+import TwistyPlayer from "@/components/cubingjs/twistyPlayer.vue";
 
 const message: Ref<string> = ref('');
 const scramble: Ref<string> = ref('');
 const playerList: Ref<string[]> = ref([]);
+const playerResults: Ref<object> = ref({});
 const messageList: Ref<object[]> = ref([]);
+const time: Ref<number> = ref(0);
+const timingState: Ref<number> = ref(0);
+const finished: Ref<boolean> = ref(false);
+const is_typing: Ref<boolean> = ref(false);  // 焦点在输入框上
+const imgVisible: Ref<boolean> = ref(true);
+const is3d: Ref<boolean> = ref(false);
 
 const roomId = router.currentRoute.value.params.roomId;
 const event = router.currentRoute.value.params.event;
-const baseUrl = "ws://localhost:8000/ws/pk/";
+//const baseUrl = "ws://127.0.0.1:8000/ws/pk/";
+const baseUrl = "wss://yougi.top/ws/pk/";
 const pkSocket = new WebSocket(baseUrl + roomId + "/" + event + "/");
+
+const set_time = (t) => {
+  time.value = t.punishment===-1?0:t.time;
+  send_finish();
+  finished.value = true;
+};
 
 pkSocket.onmessage = (event) => {
   const message = JSON.parse(event.data);
@@ -127,9 +178,13 @@ pkSocket.onmessage = (event) => {
       break;
     case 'new_round':
       scramble.value = message['scramble'];
+      finished.value = false;
       break;
     case 'player_list':
       playerList.value = message['players'];
+      break;
+    case 'finish':
+      playerResults.value[message['sender']].push(time_convert(message['time']));
       break;
     default:
       console.error('Unrecognized message type: ' + message['type']);
@@ -138,6 +193,7 @@ pkSocket.onmessage = (event) => {
 pkSocket.onclose = function(e) {
   console.error('Chat socket closed unexpectedly');
 };
+
 const send_chat_message = () => {
   if (message.value === "") {
     ElMessage.error("消息不能为空")
@@ -149,7 +205,29 @@ const send_chat_message = () => {
   }));
   message.value = "";
 };
+const send_finish = () => {
+  pkSocket.send(JSON.stringify({
+    'type': 'finish',
+    'time': time.value
+  }));
+};
+watch(playerList, (newPlayerList) => {
+  for (let player of newPlayerList) {
+    if (!(player in playerResults.value)) {
+      playerResults.value[player] = [];
+    }
+  }
+  // remove players who left
+  for (let player in playerResults.value) {
+    if (!newPlayerList.includes(player)) {
+      delete playerResults.value[player];
+    }
+  }
+});
 
+
+
+// 滚动条自动滚动到底部
 setInterval(() => {
   let scroll_bars = document.getElementsByClassName('el-scrollbar__wrap');
   let scroll_bar = scroll_bars[scroll_bars.length - 1];
@@ -223,6 +301,16 @@ setInterval(() => {
 }
 
 
+.room_right_container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+  height: 100%;
+  width: 70%;
+}
+
+
 .room_header {
   display: flex;
   flex-direction: row;
@@ -230,5 +318,70 @@ setInterval(() => {
   align-items: center;
   height: 10%;
   width: 100%;
+}
+
+.room_header__left {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100%;
+  width: 20%;
+}
+.room_header__left p:first-child {
+  font-size: 30px;
+  font-weight: bold;
+  margin-right: 20px;
+}
+.room_header__left p:last-child {
+  font-size: 20px;
+  font-weight: bold;
+  margin-right: 20px;
+}
+
+.room_header__right {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 80%;
+}
+.room_header__right p {
+  font-size: 20px;
+  font-weight: bold;
+  margin-right: 20px;
+}
+
+
+.room_main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 80%;
+  width: 100%;
+}
+.room_main__time {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+.room_main__time p {
+  font-size: 100px;
+  font-weight: bold;
+  margin-bottom: 100px;
+}
+.room_main__scramble_img {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+}
+.scramble-img-select {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
