89de751667503128c0995b3b7f4f870cf18c1130
Westlifers <2585687459@qq.com>
2023-05-14 00:38:30 +0800

    完成房间列表页新建房间功能

--- src/views/room/components/roomList.vue
+++ src/views/room/components/roomList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="room_list_container">
-    <h1>房间列表</h1>
+    <h1>房间列表&nbsp;<el-button type="primary" icon="Plus" @click="dialogVisible=true">创建房间</el-button></h1>
     <div class="room_list_table">
       <el-table :data="roomList" :header-cell-style="{'text-align':'center'}" :cell-style="{'text-align':'center'}">
         <el-table-column prop="room_name" label="房间名">
@@ -27,13 +27,64 @@
       </el-table>
     </div>
   </div>
+
+  <el-dialog title="创建房间" v-model="dialogVisible" width="30%">
+    <el-form
+        ref="formRef"
+        :model="state.form"
+        :rules="state.rules"
+    >
+      <el-form-item label="房间名" prop="room_name">
+        <el-input v-model="state.form.room_name"></el-input>
+      </el-form-item>
+      <el-form-item label="项目" prop="event">
+        <el-select v-model="state.form.event" placeholder="请选择">
+          <el-option v-for="item in ALL_EVENTS" :key="item" :label="item" :value="item"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="handleSubmit(formRef)">确 定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
 import {getRoomList} from "@/api/fetchData";
 import {Room} from "@/types";
+import {ref} from "vue";
+import {ALL_EVENTS} from "@/utils";
+import {FormInstance} from "element-plus";
+import router from "@/router";
 
 const roomList: Room[] = await getRoomList();
+const formRef = ref<FormInstance>()
+const dialogVisible = ref(false);
+const state = ref({
+  form: {
+    room_name: '',
+    event: '',
+  },
+  rules: {
+    room_name: [
+      { required: true, message: '请输入房间名', trigger: 'blur' },
+      { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+    ],
+    event: [
+      { required: true, message: '请选择项目', trigger: 'change' }
+    ]
+  }
+})
+
+const handleSubmit = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.validate(async (valid) => {
+    if (valid) {
+      await router.push(`/pk/room/${state.value.form.room_name}/${state.value.form.event}`)
+    }
+  })
+}
 </script>
 
 <style scoped>
@@ -44,6 +95,7 @@ const roomList: Room[] = await getRoomList();
   background-color: var(--yougi-projects-section);
   border-radius: 30px;
   padding-bottom: 32px;
+  height: calc(100vh - 120px);
 }
 .room_list_container h1 {
   margin-top: 32px;
